ca9812767bda80daeff84f4cf8b95e3f6d9105a9
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-17 14:58:48 -0500

    Pedir Datos Prompt

--- main.js
+++ main.js
@@ -1,32 +1,6 @@
-//Varibles//
-string = "cadenaTexto"; //Tipo Texto
-number = 24; //Tipo numero
-booleano = true //O tambien puede ser false//
+let nombre = prompt("Hola escribe tu nombre");//Con esta funcion pedimos datos a los usuarios. 
+alert("Hola " + nombre);//Mostramos en pantalla lo que se almaceno en la variable nombre;
 
-//Tipos de variables//
-var  edad = 24 // Variable tipo var declara una variable, opcionalmente inicializándola con un valor.
-let Año = 2022 // Variable tipo let permite declarar variables limitando su alcance (scope) al bloque,
-const nombrePersona = "Jeyson" // Variable tipo const va a ser siempre una varibale constante, no puede cambiar su valor.
-
-//--------Inicializar declarar y modificar Variables.........//
-let numero1; //Declaracion Variable.
-numero1 = 29; //Inicializar variable.
-alert(numero1); //Mostar en pantalla con alert.
-
-//Declara e inicializar en la misma linea.
-let numero2 = 34;
-alert(numero2);//Mostar en pantalla con alert.
-
-//crear multiples variables
-let numero3=8;
-let numero4=23;
-let numero5=103;
-
-//Tipo de dato Null,Nam;
-let curso = null; //La variable esta vacia si esta definida.
-let numeros = 5;
-let numeros2 = "pedro"; 
-alert(numeros*numeros2);// Variable numeros2 , No es un numero.
 
 
 
