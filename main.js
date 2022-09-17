d879ea4bd4c4a988c4c5034394426d48ca05075a
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-17 14:50:32 -0500

    Variables

---
+++ main.js
@@ -0,0 +1,35 @@
+//Varibles//
+string = "cadenaTexto"; //Tipo Texto
+number = 24; //Tipo numero
+booleano = true //O tambien puede ser false//
+
+//Tipos de variables//
+var  edad = 24 // Variable tipo var declara una variable, opcionalmente inicializándola con un valor.
+let Año = 2022 // Variable tipo let permite declarar variables limitando su alcance (scope) al bloque,
+const nombrePersona = "Jeyson" // Variable tipo const va a ser siempre una varibale constante, no puede cambiar su valor.
+
+//--------Inicializar declarar y modificar Variables.........//
+let numero1; //Declaracion Variable.
+numero1 = 29; //Inicializar variable.
+alert(numero1); //Mostar en pantalla con alert.
+
+//Declara e inicializar en la misma linea.
+let numero2 = 34;
+alert(numero2);//Mostar en pantalla con alert.
+
+//crear multiples variables
+let numero3=8;
+let numero4=23;
+let numero5=103;
+
+//Tipo de dato Null,Nam;
+let curso = null; //La variable esta vacia si esta definida.
+let numeros = 5;
+let numeros2 = "pedro"; 
+alert(numeros*numeros2);// Variable numeros2 , No es un numero.
+
+
+
+
+
+
