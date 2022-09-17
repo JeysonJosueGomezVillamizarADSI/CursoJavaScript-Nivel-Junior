6c45a5c7c32ba23879b931b10d3df09c23f48c4d
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-17 16:49:57 -0500

    Operadores

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-let nombre = prompt("Hola escribe tu nombre");//Con esta funcion pedimos datos a los usuarios. 
-alert("Hola " + nombre);//Mostramos en pantalla lo que se almaceno en la variable nombre;
+//Operadores Asignacion
+let numeros1 = 10;
+numeros1 += 5; //Asignacion Adicion
+document.write(numeros1)
 
+let numeros2 = 10;
+numeros2 -= 5; //Asignacion Sutraccion
+alert(numeros2)
 
+let numeros3 = 10;
+numeros3 *= 5; //Asignacion Multiplicacion
+alert(numeros3)
 
+let numeros4 = 10;
+numeros4 /= 5; //Asignacion Division
+alert(numeros4);
 
+let numeros5 = 50;
+numeros5 %= 6; //Asignacion de resto Muestra el resto de una division.
+alert(numeros5);
 
+let numeros6 = 10;
+numeros6 **= 5; //Asignacion de Exponenciasion.
+alert(numeros6);
 
-
+//Operadores Aritmeticos
+let numero1= 10;
+let numero2 = 5;
+resultado = numero1+numero2; //Adicion.
+alert(resultado);
