9ada4be8d35c0fdaa2336b044f208a3d84356904
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-10-02 16:30:57 -0500

    Operadores de Comparacion

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-//*Concatenar con el signo + :
-//saludo="Hola Jeyson";
-//pregunta="¿Como estas?";
-//frase1=saludo + pregunta;
-//document.write(frase1);
+//Operadores de comparacion:
+//Igualdad
+//let numero=23;
+//let numero2=13;
+//document.write(numero==numero2)
 
-//Concatenar numeros con texto: forzamos string("")
-//numer1=545;
-//numer2=8;
-//frase2="" + numer1 + numer2;
-//document.write(frase2)
+//Distinto
+//let numero=23;
+//let numero2=13;
+//document.write(numero!=numero2);
 
-//Concatenar con concat:debemos como minimo tener una variable string
-//numero1="53";
-//numero2=7;
-//frase3=numero1.concat(numero2);
-//document.write(frase3);
+//Identidad : === estrictamente iguales
+//let numero=23;
+//let numero2=13;
+//let texto="23";
+//let texto2="texto 2";
+//document.write(texto===numero);
 
-//Concatenar con backtiks y la variable entre ${}
-nombre="Jeyson Gomez";
-frase=`Soy ${nombre} y estoy programando`;
-document.write(frase);
+//No identidad : !== no es estrictamente igual
+//let numero=23;
+//let numero2=13;
+//let texto="23";
+//let texto2="texto 2";
+//document.write(texto!==numero);
+
+//Mayor que y mayor igual que
+//let numero=23;
+//let numero2=13;
+//document.write(numero>numero2);
+let numero=23;
+let numero2=13;
+document.write(numero>=numero2);
