0f4ac4fb23a7403b9189dc5e0c1febd25c616446
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-10-02 12:14:42 -0500

    Concatenar

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-//Operadores Asignacion
-let numeros1 = 10;
-numeros1 += 5; //Asignacion Adicion
-document.write(numeros1)
+//*Concatenar con el signo + :
+//saludo="Hola Jeyson";
+//pregunta="¿Como estas?";
+//frase1=saludo + pregunta;
+//document.write(frase1);
 
-let numeros2 = 10;
-numeros2 -= 5; //Asignacion Sutraccion
-alert(numeros2)
+//Concatenar numeros con texto: forzamos string("")
+//numer1=545;
+//numer2=8;
+//frase2="" + numer1 + numer2;
+//document.write(frase2)
 
-let numeros3 = 10;
-numeros3 *= 5; //Asignacion Multiplicacion
-alert(numeros3)
+//Concatenar con concat:debemos como minimo tener una variable string
+//numero1="53";
+//numero2=7;
+//frase3=numero1.concat(numero2);
+//document.write(frase3);
 
-let numeros4 = 10;
-numeros4 /= 5; //Asignacion Division
-alert(numeros4);
-
-let numeros5 = 50;
-numeros5 %= 6; //Asignacion de resto Muestra el resto de una division.
-alert(numeros5);
-
-let numeros6 = 10;
-numeros6 **= 5; //Asignacion de Exponenciasion.
-alert(numeros6);
-
-//Operadores Aritmeticos
-let numero1= 10;
-let numero2 = 5;
-resultado = numero1+numero2; //Adicion.
-alert(resultado);
+//Concatenar con backtiks y la variable entre ${}
+nombre="Jeyson Gomez";
+frase=`Soy ${nombre} y estoy programando`;
+document.write(frase);
